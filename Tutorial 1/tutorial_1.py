f9453a3905e7afb36b84bcf227886ba7efd78dbb
Kaveen Seneviratne <kaveenseneviratne.work@gmail.com>
2026-01-27 22:40:59 +0100

    tutorial 1 added

---
+++ Tutorial 1/tutorial_1.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+
+# importing the fashion mnist dataset from keras
+data = keras.datasets.fashion_mnist
+
+# splitting data into training and testing splits
+
