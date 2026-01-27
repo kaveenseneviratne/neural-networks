d6f2ec7000b819b33e2a34ae1b08d4fa328b070d
Kaveen Seneviratne <kaveenseneviratne.work@gmail.com>
2026-01-27 22:51:31 +0100

    tutorial 1 data imported

--- Tutorial 1/tutorial_1.py
+++ Tutorial 1/tutorial_1.py
@@ -7,4 +7,16 @@ import matplotlib.pyplot as plt
 data = keras.datasets.fashion_mnist
 
 # splitting data into training and testing splits
+(train_images, train_labels),(test_images,test_labels) = data.load_data()
 
+# defining what the labels in the dataset are
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+# scaling the data
+train_images = train_images/255
+test_images = test_images/255
+
+# checking one specific example of the data
+plt.imshow(train_images[7], cmap = plt.cm.binary)
+plt.show()
