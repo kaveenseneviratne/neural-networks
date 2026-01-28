6127926ece08795e6a4851a86372f857153f0e22
Kaveen Seneviratne <kaveenseneviratne.work@gmail.com>
2026-01-28 01:31:16 +0100

    tutorial_1.py modified

--- Tutorial 1/tutorial_1.py
+++ Tutorial 1/tutorial_1.py
@@ -13,10 +13,18 @@ data = keras.datasets.fashion_mnist
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
-# scaling the data
-train_images = train_images/255
-test_images = test_images/255
+# initiating the neural network
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape = (28,28)), # input layer flattened
+    keras.layers.Dense(128,activation="relu"), # hidden layer
+    keras.layers.Dense(10,activation="softmax") # output layer
+])
 
-# checking one specific example of the data
-plt.imshow(train_images[7], cmap = plt.cm.binary)
-plt.show()
+model.compile(optimizer="adam",loss="sparse_categorical_crossentropy",metrics=["accuracy"])
+
+# training our model
+model.fit(train_images,train_labels,epochs=25)
+
+# testing the model on the test data
+test_loss, test_accuracy = model.evaluate(test_images,test_labels)
+print("Tested Accuracy:", test_accuracy)
