566ac6baf1464956f1a6ae03bbf859030bcd3d4b
Kaveen Seneviratne <kaveenseneviratne.work@gmail.com>
2026-01-29 22:45:30 +0100

    neural network is done and has an accuracy of 87%

--- Text Classification/text_classification.py
+++ Text Classification/text_classification.py
@@ -26,7 +26,25 @@ def decode_review(text):
 
 # defining the model
 model = keras.Sequential()
-model.add(keras.layers.Embedding(10000,16))
+model.add(keras.layers.Embedding(10000,16)) # 
 model.add(keras.layers.GlobalAveragePooling1D())
 model.add(keras.layers.Dense(16,activation="relu"))
-model.add(keras.layers.Embedding(1,activation="sigmoid"))
+model.add(keras.layers.Dense(1,activation="sigmoid"))
+
+model.summary()
+
+model.compile(optimizer="adam",loss="binary_crossentropy",metrics=["accuracy"])
+
+x_val = train_data[:10000] # creating a dataset as validation data
+x_train = train_data[10000:]
+
+y_val = train_labels[:10000]
+y_train = train_labels[10000:]
+
+fitModel = model.fit(x_train,y_train,epochs=40,batch_size=512,validation_data=(x_val,y_val),verbose=1)
+
+results = model.evaluate(test_data,test_labels)
+
+print(results)
+
+# making predictions from the model
